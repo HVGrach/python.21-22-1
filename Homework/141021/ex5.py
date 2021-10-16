2c31a6fd02f041b82a3c7cf77c255ea3f2f6db75
Lisa <kulakova.elizaveta0509@gmail.com>
2021-10-16 22:02:11 +0300

    141021 сделала арифметику

---
+++ Homework/141021/ex5.py
@@ -0,0 +1,11 @@
+import math
+
+a=int(input())
+b=int(input())
+print("Сумма:", a+b)
+print("Разность:", a-b)
+print("Произведение:", a*b)
+print("Частное от деления:", a//b)
+print("Остаток от деления:", a%b)
+print("Десятичный логарифм:", math.log10(a))
+print("Степень:", a**b)

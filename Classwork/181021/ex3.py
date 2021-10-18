4fff34b332fb9e4bf8f2d327270f1b1914046baf
Lisa <kulakova.elizaveta0509@gmail.com>
2021-10-18 12:48:21 +0300

    181021 Задание3

---
+++ Classwork/181021/ex3.py
@@ -0,0 +1,9 @@
+let=str(input('Буква латинского алфавита'))
+if let=='a' or let=='e' or let=='i' or let=='o' or let=='u':
+    print('Гласная')
+elif let=='y':
+    print('И гласная, и согласная')
+else:
+    print('Согласная')
+
+

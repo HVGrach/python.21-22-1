f637935235c92b5a8ba2199c8c33c9350be66d92
Lisa <kulakova.elizaveta0509@gmail.com>
2021-11-22 18:52:45 +0300

    151121 3 Задание

---
+++ Homework/November/151121/ex3.py
@@ -0,0 +1,16 @@
+import random
+mon = [1, 2]
+a = 0
+tries = 0
+while a != 3:  # пока не наберется 3 орла/решки
+    ran = random.choice(mon)
+    if ran == 1:
+        a += 1
+        print("Р", end=" ")
+        tries += 1
+    else:
+        a += 0
+        print("О", end=" ")
+        tries += 1
+
+print("Попытки: ", tries)

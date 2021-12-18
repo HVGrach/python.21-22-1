e976d0f3021d607e49f5adea0007d833e199bc78
Bormotoon <bormotoon@gmail.com>
2021-12-18 09:29:19 +0300

    18.11.2021 evaluation: 5.

--- Homework/November/181121/ex2.py
+++ Homework/November/181121/ex2.py
@@ -1,14 +1,16 @@
-bin_num   =   int ( input ( "Введите двоичное число: " ))
-sum   =   ''
-sum2   =   ''
+bin_num = int(input("Введите двоичное число: "))
+sum = ''
+sum2 = ''
 # Номер цифры с строке
-index   =   0
+index = 0
 # Вывод метода расстановки степеней
-for   i   in   range ( len ( str ( bin_num ))  -   1 ,  - 1 ,  - 1 ):
-      sum   +=   f" { list ( str ( bin_num ))[ index ] } ∙2^ { str ( i ) }  + "
-      sum2   +=   f" { int ( list ( str ( bin_num ))[ index ])  *  ( 2   **   i ) }  + "
-      index   +=   1
-print ( f" { bin_num } (2) =  { sum [ 0 : - 2 ] } =  { sum2 [ 0 : - 2 ] } =  { int ( str ( bin_num ),  2 ) } (10)" 
-         f" \n " 
-         f" \n Ответ: " 
-         f" \n { bin_num } (2) =  { int ( str ( bin_num ),  2 ) } (10)" )
+for i in range(len(str(bin_num)) - 1, - 1, - 1):
+    sum += f" {list(str(bin_num))[index]} ∙2^ {str(i)}  + "
+    sum2 += f" {int(list(str(bin_num))[index]) * (2 ** i)}  + "
+    index += 1
+print(f" {bin_num} (2) =  {sum[0: - 2]} =  {sum2[0: - 2]} =  {int(str(bin_num), 2)} (10)"
+      f" \n "
+      f" \n Ответ: "
+      f" \n {bin_num} (2) =  {int(str(bin_num), 2)} (10)")
+
+# Evaluation: OK. Съехали отступы. И что это за дичь с пробелами? О_о
--- Homework/November/181121/ex3.py
+++ Homework/November/181121/ex3.py
@@ -1,16 +1,19 @@
-import   random
-changes   =   0
-max_num   =   0
-i   =   0
-while   max_num   !=   100 :
+import random
+
+changes = 0
+max_num = 0
+i = 0
+while max_num != 100:
     # Вывод рандомных чисел
-    num   =   random . randint ( 0 ,  100 )
+    num = random.randint(0, 100)
     # Поиск максимального числа. "i" нужно для того, чтобы первое число не считалось обновлением
-    if   num   >   max_num   and   i   !=   0 :
-           max_num   =   num
-           changes   +=   1
-           print ( f" { num }  <== Обновление" )
-    else :
-           print ( num )
+    if num > max_num and i != 0:
+        max_num = num
+        changes += 1
+        print(f" {num}  <== Обновление")
+    else:
+        print(num)
     i += 1
-print ( f"Максимальное значение в ряду: 100 "  f" \n Количество смен максимального значения:  { changes } " )
+print(f"Максимальное значение в ряду: 100 "  f" \n Количество смен максимального значения:  {changes} ")
+
+# Evaluation: OK. Снова отступы и снова пробелы. Дичь какая.
